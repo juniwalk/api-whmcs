4eb537e620d568fba83ffa6a641ea2deb2d85490
juniwalk <juniwalk@outlook.cz>
2021-04-12 14:09:51 +0200

    Added connector and client classes

---
+++ src/Connector.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2021
+ * @license   MIT License
+ */
+
+namespace JuniWalk\WHMCS;
+
+class Connector
+{
+	use Subsystems\Client;
+}
---
+++ src/Subsystems/Client.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2021
+ * @license   MIT License
+ */
+
+namespace JuniWalk\WHMCS\Subsystems;
+
+trait Client
+{
+	public function getClient()
+	{
+		return 'client';
+	}
+}
