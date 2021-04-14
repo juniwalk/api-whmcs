fe517824644807fe490cc8b73c109249dac4eea7
juniwalk <juniwalk@outlook.cz>
2021-04-14 09:54:01 +0200

    Added entity getters and setters

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -32,4 +32,117 @@ class Client extends AbstractEntity
 
 	/** @var string */
 	protected $phonenumber;
+
+
+	/**
+	 * @return int
+	 */
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getGUID(): string
+	{
+		return $this->uuid;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getFirstName(): string
+	{
+		return $this->firstname;
+	}
+
+
+	/**
+	 * @param  string  $firstName
+	 * @return void
+	 */
+	public function setFirstName(string $firstName): void
+	{
+		$this->firstname = $firstName;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getLastName(): string
+	{
+		return $this->lastname;
+	}
+
+
+	/**
+	 * @param  string  $lastName
+	 * @return void
+	 */
+	public function setLastName(string $lastName): void
+	{
+		$this->lastname = $lastName;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getCompanyName(): string
+	{
+		return $this->companyname;
+	}
+
+
+	/**
+	 * @param  string  $companyName
+	 * @return void
+	 */
+	public function setCompanyName(string $companyName): void
+	{
+		$this->companyname = $companyName;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getEmail(): string
+	{
+		return $this->email;
+	}
+
+
+	/**
+	 * @param  string  $email
+	 * @return void
+	 */
+	public function setEmail(string $email): void
+	{
+		$this->email = $email;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getPhoneNumber(): string
+	{
+		return $this->phonenumber;
+	}
+
+
+	/**
+	 * @param  string  $phoneNumber
+	 * @return void
+	 */
+	public function setPhoneNumber(string $phoneNumber): void
+	{
+		$this->phonenumber = $phoneNumber;
+	}
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -29,4 +29,108 @@ class Product extends AbstractEntity
 
 	/** @var float */
 	protected $overagesdiskprice;
+
+
+	/**
+	 * @return int
+	 */
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getType(): string
+	{
+		return $this->type;
+	}
+
+
+	/**
+	 * @param  string  $type
+	 * @return void
+	 */
+	public function setType(string $type): void
+	{
+		$this->type = $type;
+	}
+
+
+	/**
+	 * @return int
+	 */
+	public function getGroupId(): int
+	{
+		return $this->gid;
+	}
+
+
+	/**
+	 * @param  int  $groupId
+	 * @return void
+	 */
+	public function setGroupId(int $groupId): void
+	{
+		$this->gid = $groupId;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+
+	/**
+	 * @param  string  $name
+	 * @return void
+	 */
+	public function setName(string $name): void
+	{
+		$this->name = $name;
+	}
+
+
+	/**
+	 * @return int
+	 */
+	public function getOveragesDiskLimit(): int
+	{
+		return $this->overagesdisklimit;
+	}
+
+
+	/**
+	 * @param  int  $limit
+	 * @return void
+	 */
+	public function setOveragesDiskLimit(int $limit): void
+	{
+		$this->overagesdisklimit = $limit;
+	}
+
+
+	/**
+	 * @return float
+	 */
+	public function getOveragesDiskPrice(): float
+	{
+		return $this->overagesdiskprice;
+	}
+
+
+	/**
+	 * @param  int  $price
+	 * @return void
+	 */
+	public function setOveragesDiskPrice(int $price): void
+	{
+		$this->overagesdiskprice = $price;
+	}
 }
