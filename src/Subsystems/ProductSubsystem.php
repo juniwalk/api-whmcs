ea9931b7e5df41259b380ec87ecb2161542d5da8
juniwalk <juniwalk@outlook.cz>
2021-04-19 11:08:37 +0200

    Added method to load expansion of the hosting

--- src/Subsystems/ProductSubsystem.php
+++ src/Subsystems/ProductSubsystem.php
@@ -7,6 +7,7 @@
 
 namespace JuniWalk\WHMCS\Subsystems;
 
+use JuniWalk\WHMCS\Entity\Hosting;
 use JuniWalk\WHMCS\Entity\Product;
 
 trait ProductSubsystem
@@ -28,5 +29,23 @@ trait ProductSubsystem
 	public function findProduct(int $productId): ?Product
 	{
 		return $this->findOneById($productId, Product::class);
+	}
+
+
+	/**
+	 * @param  int  $userId
+	 * @param  int  $productGroupId
+	 * @return Product|null
+	 */
+	public function findActiveDiskExpansion(int $userId, int $productGroupId): ?Product
+	{
+		return $this->findOneBy(Product::class, function($qb) use ($userId, $productGroupId) {
+			$qb->innerJoin('e', Hosting::TABLE_NAME, 'h', 'h.packageid = e.id');
+			$qb->where('e.gid = :group AND e.type = :type AND h.domainstatus = :status AND h.userid = :user');
+			$qb->setParameter('group', $productGroupId);
+			$qb->setParameter('status', 'Active');
+			$qb->setParameter('user', $userId);
+			$qb->setParameter('type', 'other');
+		});
 	}
 }
