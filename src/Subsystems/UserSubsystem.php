c37a047044d3178878fa699c28c886fa8f55453e
juniwalk <juniwalk@outlook.cz>
2022-03-09 13:18:34 +0100

    Do not use php8.1 yet

--- src/Subsystems/UserSubsystem.php
+++ src/Subsystems/UserSubsystem.php
@@ -7,27 +7,27 @@
 
 namespace JuniWalk\WHMCS\Subsystems;
 
-use JuniWalk\WHMCS\Enums\Sort;
+// use JuniWalk\WHMCS\Enums\Sort;
 
 trait UserSubsystem
 {
 	/**
 	 * @param  string|null  $search
-	 * @param  Sort  $sort
+	 * @param  string  $sort
 	 * @param  int  $offset
 	 * @param  int  $limit
 	 * @return string[]
 	 */
 	public function getUsers(
 		string $search = null,
-		Sort $sort = Sort::ASC,
+		string $sort = 'ASC',
 		int $offset = 0,
 		int $limit = 25
 	): iterable {
 		return $this->call('GetUsers', [
 			'limitstart' => $offset,
 			'limitnum' => $limit,
-			'sorting' => $sort->name,
+			'sorting' => $sort,
 			'search' => $search,
 		]);
 	}
