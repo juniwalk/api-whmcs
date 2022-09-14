4c409ef2ae7f684530578e003ab4874bbe9ca7ca
juniwalk <juniwalk@outlook.cz>
2022-09-14 14:26:20 +0200

    Added Link::createClientLink method

--- src/Subsystems/LinkSubsystem.php
+++ src/Subsystems/LinkSubsystem.php
@@ -9,6 +9,12 @@ namespace JuniWalk\WHMCS\Subsystems;
 
 trait LinkSubsystem
 {
+	public function createClientLink(int $clientId): string
+	{
+		return $this->url.'/'.$this->adminDir.'/clientssummary.php?userid='.$clientId;
+	}
+
+
 	public function createInvoiceLink(int $invoiceId): string
 	{
 		return $this->url.'/'.$this->adminDir.'/invoices.php?action=edit&id='.$invoiceId;
