deb6fe0b04f801f6ffc4e43d85a126bddcf7b41a
juniwalk <juniwalk@outlook.cz>
2022-11-03 13:36:02 +0100

    Added new request exception

---
+++ src/Exceptions/RequestException.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2022
+ * @license   MIT License
+ */
+
+namespace JuniWalk\WHMCS\Exceptions;
+
+use Throwable;
+
+final class RequestException extends WHMCSException
+{
+	public static function fromAction(string $action, Throwable $e = null): static
+	{
+		return new static('Request of '.$action.' in WHMCS api has failed.', 0, $e);
+	}
+}
