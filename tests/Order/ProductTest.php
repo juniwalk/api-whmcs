4d0755c411267b006229229d07fc6ebac73dcba9
juniwalk <juniwalk@outlook.cz>
2024-06-13 14:43:04 +0200

    Use nullsafe call as item could be missing

--- tests/Order/ProductTest.php
+++ tests/Order/ProductTest.php
@@ -24,9 +24,9 @@ final class ProductTest extends TestCase
 		$whmcs = getConfig()->createConnector();
 		$items = $whmcs->getProducts(productId: 139);
 
-		Assert::same($items[0]->getType(), 'other');
-		Assert::same($items[0]->getPackageId(), 139);
-		// Assert::null($items[0]->getGroupName());	// TODO: Uncomment when iterating over properties in AbstractEntity is implemented
+		Assert::same($items[0]?->getType(), 'other');
+		Assert::same($items[0]?->getPackageId(), 139);
+		// Assert::null($items[0]?->getGroupName());	// TODO: Uncomment when iterating over properties in AbstractEntity is implemented
 	}
 }
 
